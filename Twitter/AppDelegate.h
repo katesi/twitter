9c2d73874c558ca6940ce7aeb8104382afd04329
katesi <katesi@fb.com>
2015-02-25 10:19:09 -0800

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Katerina Simonova on 2/25/15.
+//  Copyright (c) 2015 Katerina Simonova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
